5df9270ec5a79e215b813de0bbb27db74964d0ae
stargazing <huder13168@gmail.com>
2021-05-12 13:57:53 -0500

    formulario creado

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,10 +1,32 @@
-import React,{useState} from 'react' 
+import React, { useState } from "react";
 
-const Formulario = ()=> {
+const Formulario = () => {
+  return (
+    <div className="container">
+      <h1>CRUD BÁSICO</h1>
 
-    return(
-        <h1>CRUD BÁSICO</h1>
-    )
-}
+      {/* CREA LOS CONTENEDORES */}
+      <div className="row">
+        {/* VISTA */}
+        <div className="col">
+          <h2>Lista de Usuarios</h2>
+        </div>
 
-export default Formulario
+        {/* AGREGANDO A LOS USUARIOS */}
+        <div className="col">
+          <h2>Formulario para añadir nombres</h2>
+
+          {/* formulario */}
+          <form>
+            <div>
+              <input className="form-control" type="text" />
+            </div>
+            <button className="btn btn-primary">Registrar</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Formulario;
